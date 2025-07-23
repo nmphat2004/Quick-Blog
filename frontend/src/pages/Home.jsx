f19fbd6e08304a02922550b7e36b94127056ea6d
Phat <2251120435@ut.edu.vn>
2025-07-23 23:25:38 +0700

    add footer - done home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import BlogList from '../components/BlogList';
 import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 const Home = () => {
 	return (
@@ -10,6 +11,7 @@ const Home = () => {
 			<Header />
 			<BlogList />
 			<Newsletter />
+			<Footer />
 		</>
 	);
 };
