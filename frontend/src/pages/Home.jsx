f57fe698d2568089ce67ef872a02da50ad4188c4
Phat <2251120435@ut.edu.vn>
2025-07-23 22:51:48 +0700

    add newsletter

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import BlogList from '../components/BlogList';
+import Newsletter from '../components/Newsletter';
 
 const Home = () => {
 	return (
@@ -8,6 +9,7 @@ const Home = () => {
 			<Navbar />
 			<Header />
 			<BlogList />
+			<Newsletter />
 		</>
 	);
 };
