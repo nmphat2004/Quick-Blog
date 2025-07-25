1a47f222dc4ea47a36b53efa0c0835b0dd1434c9
Phat <2251120435@ut.edu.vn>
2025-07-25 17:46:11 +0700

    add Login component

--- frontend/src/components/admin/Sidebar.jsx
+++ frontend/src/components/admin/Sidebar.jsx
@@ -28,14 +28,14 @@ const Sidebar = () => {
 			</NavLink>
 
 			<NavLink
-				to='/admin/blog_list'
+				to='/admin/list_blog'
 				className={({ isActive }) =>
 					`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${
 						isActive && 'bg-primary/10 border-r-4 border-primary'
 					}`
 				}>
 				<img src={assets.list_icon} alt='' />
-				<p>Blog List</p>
+				<p>List Blog</p>
 			</NavLink>
 
 			<NavLink
