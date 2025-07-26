db60d93346da0c7d3ac61f1da197a510e3be0c67
Phat <2251120435@ut.edu.vn>
2025-07-26 23:16:05 +0700

    render list blog admin

--- frontend/src/pages/admin/ListBlog.jsx
+++ frontend/src/pages/admin/ListBlog.jsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { blog_data } from '../../assets/assets';
+import BlogTableItem from '../../components/admin/BlogTableItem';
 
 const ListBlog = () => {
-	return <div>ListBlog</div>;
+	const [blogs, setBlogs] = useState([]);
+
+	const fetchBlogs = async () => {
+		setBlogs(blog_data);
+	};
+
+	useEffect(() => {
+		fetchBlogs();
+	});
+
+	return (
+		<div className='flex-1 px-5 pt-5 sm:pt-12 sm:pl-16 bg-blue-50/50'>
+			<h1>All blogs</h1>
+
+			<div className='relative h-4/5 mt-4 max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white'>
+				<table className='w-full text-sm text-gray-500 relative'>
+					<thead className='text-xs text-gray-600 text-left uppercase top-0 sticky z-10 bg-white'>
+						<tr>
+							<th scope='col' className='px-2 py-4 xl:px-6'>
+								#
+							</th>
+							<th scope='col' className='px-2 py-4 xl:px-6'>
+								Blog Title
+							</th>
+							<th scope='col' className='px-2 py-4 xl:px-6 max-sm:hidden'>
+								Date
+							</th>
+							<th scope='col' className='px-2 py-4 xl:px-6 max-sm:hidden'>
+								Status
+							</th>
+							<th scope='col' className='px-2 py-4 xl:px-6'>
+								Actions
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{blogs.map((blog, index) => {
+							return (
+								<BlogTableItem
+									key={blog.id}
+									blog={blog}
+									fetchBlogs={fetchBlogs}
+									index={index + 1}
+								/>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 };
 
 export default ListBlog;
