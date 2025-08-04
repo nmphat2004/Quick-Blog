350dff8253c3662289517eaa8aaa747502916e80
Phat <2251120435@ut.edu.vn>
2025-08-05 00:09:33 +0700

    install quill and create add blog page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import AddBlog from './pages/admin/AddBlog';
 import Comments from './pages/admin/Comments';
 import ListBlog from './pages/admin/ListBlog';
 import Login from './components/admin/Login';
+import 'quill/dist/quill.snow.css';
 
 const App = () => {
 	return (
--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -1,7 +1,109 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { assets, blog_categories } from '../../assets/assets';
+import Quill from 'quill';
 
 const AddBlog = () => {
-	return <div>AddBlog</div>;
+	const [image, setImage] = useState(false);
+	const [title, setTitle] = useState('');
+	const [subTitle, setSubTitle] = useState('');
+	const [Category, setCategory] = useState('');
+	const [isApproved, setIsApproved] = useState('');
+
+	const editorRef = useRef(null);
+	const quillRef = useRef(null);
+
+	const handleSubmit = (e) => {
+		e.prevenDefault();
+	};
+
+	const generateContent = () => {
+		// Generate Content
+	};
+
+	useEffect(() => {
+		if (!quillRef.current && editorRef.current)
+			quillRef.current = new Quill(editorRef.current, { theme: 'snow' });
+	}, []);
+
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+			<div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded'>
+				<p>Upload Thumbnail</p>
+				<label htmlFor='image'>
+					<img
+						src={!image ? assets.upload_area : URL.createObjectURL(image)}
+						alt=''
+						className='mt-2 h-16 rounded cursor-pointer'
+					/>
+					<input
+						onChange={(e) => setImage(e.target.files[0])}
+						type='file'
+						id='image'
+						hidden
+						required
+					/>
+				</label>
+				<p className='mt-4'>Blog Title</p>
+				<input
+					type='text'
+					onChange={(e) => setTitle(e.target.value)}
+					className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded'
+					value={title}
+					placeholder='Type here'
+				/>
+				<p className='mt-4'>Sub title</p>
+				<input
+					type='text'
+					onChange={(e) => setSubTitle(e.target.value)}
+					className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded'
+					value={subTitle}
+					placeholder='Type here'
+				/>
+				<p className='mt-4'>Blog Description</p>
+				<div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative'>
+					<div ref={editorRef}></div>
+					<button
+						className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer'
+						onClick={generateContent}
+						type='button'>
+						Generate with AI
+					</button>
+				</div>
+				<p className='mt-4'>Blog Category</p>
+				<select
+					onChange={(e) => setCategory(e.target.value)}
+					name='category'
+					className='mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded'>
+					<option value='' selected disabled>
+						-- Select Category --
+					</option>
+					{blog_categories.map((item, index) => {
+						return (
+							<option key={index} value={item}>
+								{item}
+							</option>
+						);
+					})}
+				</select>
+				<div className='flex gap-2 mt-4'>
+					<p>Publish Now</p>
+					<input
+						onChange={(e) => setIsApproved(e.target.checked)}
+						type='checkbox'
+						checked={isApproved}
+						className='scale-125 cursor-pointer'
+					/>
+				</div>
+				<button
+					type='submit'
+					className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'>
+					Add Blog
+				</button>
+			</div>
+		</form>
+	);
 };
 
 export default AddBlog;
