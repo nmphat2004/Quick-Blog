78d055f1476de237fd98fd6eec21c8bbfa268c4b
Phat <2251120435@ut.edu.vn>
2025-08-12 22:29:55 +0700

    add context provider, integrate axios for API calls, login admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,20 +8,27 @@ import Comments from './pages/admin/Comments';
 import ListBlog from './pages/admin/ListBlog';
 import Login from './components/admin/Login';
 import 'quill/dist/quill.snow.css';
+import { Toaster } from 'react-hot-toast';
+import { useAppContext } from './context/AppContext';
 
 const App = () => {
+	const { token } = useAppContext();
+
 	return (
-		<Routes>
-			<Route path='/' element={<Home />} />
-			<Route path='/blog/:id' element={<Blog />} />
-			{/* eslint-disable-next-line no-constant-condition */}
-			<Route path='/admin' element={true ? <Layout /> : <Login />}>
-				<Route index element={<Dashboard />} />
-				<Route path='add_blog' element={<AddBlog />} />
-				<Route path='list_blog' element={<ListBlog />} />
-				<Route path='comments' element={<Comments />} />
-			</Route>
-		</Routes>
+		<div>
+			<Toaster />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/blog/:id' element={<Blog />} />
+				{/* eslint-disable-next-line no-constant-condition */}
+				<Route path='/admin' element={token ? <Layout /> : <Login />}>
+					<Route index element={<Dashboard />} />
+					<Route path='add_blog' element={<AddBlog />} />
+					<Route path='list_blog' element={<ListBlog />} />
+					<Route path='comments' element={<Comments />} />
+				</Route>
+			</Routes>
+		</div>
 	);
 };
 
