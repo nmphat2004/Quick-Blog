fc6e15a91f2755489540650f86c1ad04225f98f5
Phat <2251120435@ut.edu.vn>
2025-08-14 13:35:43 +0700

    get, delete and toggle publish in admin

--- frontend/src/components/admin/BlogTableItem.jsx
+++ frontend/src/components/admin/BlogTableItem.jsx
@@ -1,9 +1,51 @@
+import toast from 'react-hot-toast';
 import { assets } from '../../assets/assets';
+import { useAppContext } from '../../context/AppContext';
 
 // eslint-disable-next-line no-unused-vars
 const BlogTableItem = ({ blog, fetchBlogs, index }) => {
 	const { title, createdAt } = blog;
 	const BlogDate = new Date(createdAt);
+	const { axios } = useAppContext();
+
+	const deleteBlog = async () => {
+		const confirm = window.confirm(
+			'Are you sure you want to delete this blog?'
+		);
+
+		if (!confirm) return;
+
+		try {
+			const { data } = await axios.post('/api/v1/blog/delete', {
+				id: blog._id,
+			});
+			if (data.success) {
+				toast.success(data.message);
+				await fetchBlogs();
+			} else {
+				toast.error(data.message);
+			}
+		} catch (error) {
+			toast.error(error.message);
+		}
+	};
+
+	const togglePublish = async () => {
+		try {
+			const { data } = await axios.post('/api/v1/blog/toggle-publish', {
+				id: blog._id,
+			});
+
+			if (data.success) {
+				toast.success(data.message);
+				await fetchBlogs();
+			} else {
+				toast.error(data.message);
+			}
+		} catch (error) {
+			toast.error(error.message);
+		}
+	};
 
 	return (
 		<tr className='border-y border-gray-300'>
@@ -19,10 +61,13 @@ const BlogTableItem = ({ blog, fetchBlogs, index }) => {
 				</p>
 			</td>
 			<td className='px-2 py-4 flex text-xs gap-3'>
-				<button className='border px-2 py-0.5 mt-1 rounded cursor-pointer'>
+				<button
+					onClick={togglePublish}
+					className='border px-2 py-0.5 mt-1 rounded cursor-pointer'>
 					{blog.isPublished ? 'Unpublish' : 'Publish'}
 				</button>
 				<img
+					onClick={deleteBlog}
 					src={assets.cross_icon}
 					alt=''
 					className='w-8 hover:scale-110 transition-all cursor-pointer'
