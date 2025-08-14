1aa90fe1c41db990a35274fe1c361b91a4c4d6bf
Phat <2251120435@ut.edu.vn>
2025-08-14 20:50:06 +0700

    get dashboard data

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
-import { assets, dashboard_data } from '../../assets/assets';
+import { assets } from '../../assets/assets';
 import BlogTableItem from '../../components/admin/BlogTableItem';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const Dashboard = () => {
 	const [dashboardData, setDashboardData] = useState({
@@ -11,7 +13,15 @@ const Dashboard = () => {
 	});
 
 	const fetchDashboardData = async () => {
-		setDashboardData(dashboard_data);
+		try {
+			const { data } = await axios.get('/api/v1/admin/dashboard');
+
+			data.success
+				? setDashboardData(data.dashboardData)
+				: toast.error(data.message);
+		} catch (error) {
+			toast.error(error.message);
+		}
 	};
 
 	useEffect(() => {
